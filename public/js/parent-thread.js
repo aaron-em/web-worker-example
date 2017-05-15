d0a6ffc34b32657cf25a6e0afa43672e3a7fe631
Aaron Miller <me@aaron-miller.me>
2017-05-15 12:45:22 -0400

    validate feeding data back from worker

--- public/js/parent-thread.js
+++ public/js/parent-thread.js
@@ -1,6 +1,8 @@
 var workerId = 0
   , logDiv;
 
+window.Workers = [];
+
 /**
  * Set up the button with a click listener which will spawn a new
  * worker.
@@ -18,7 +20,11 @@ window.addEventListener('load', function() {
  */
 function startWorker() {
   var worker = new Worker('js/worker-thread.js')
-    , id = ++workerId;
+  , id = ++workerId
+  , feeding = false
+  , buf = null
+  , fub = null
+  , expect = 0;
 
   // The worker waits for this message before it starts doing things
   worker.postMessage({'state': 'ready', 'id': id});
@@ -26,7 +32,41 @@ function startWorker() {
   // This listener fires when the worker sends us a message
   worker.addEventListener('message', log);
 
+  worker.addEventListener('message', function(evt) {
+    if (feeding) {
+      var start = 25000 * (evt.data.percent - 1);
+      var end = 25000 * evt.data.percent;
+      for (var i = start; i < end; i++) {
+        fub[i] = evt.data.content[i];
+      };
+    };
+
+    if (!feeding && evt.data.message.match(/feed size: (\d+)/)) {
+      expect = parseInt(evt.data.message.split(/\:\s+/)[1]);
+    };
+    
+    if (!feeding && evt.data.message === 'feeding content') {
+      feeding = true;
+      buf = new ArrayBuffer(250 * 1000 * 1000);
+      fub = new Uint8Array(buf);
+    };
+  });
+  
+  worker.addEventListener('message', function(evt, parent) {
+    if (evt.data.message === 'shutdown') {
+      window.Workers.splice(window.Workers.indexOf(worker), 1);
+      log({data: {id: id, message: 'ended up eating ' + fub.length}}, true);
+      log({data: {id: id,
+                  message: (fub.length === expect
+                            ? 'got'
+                            : 'did not get')
+                  + ' what was expected'}}, true);
+    };
+  });
+
   log({data:{id:id,message:'starting worker ' + id}}, true);
+
+  window.Workers.push(worker);
 };
 
 /**
--- public/js/worker-thread.js
+++ public/js/worker-thread.js
@@ -1,6 +1,8 @@
 var interval
   , id
-  , percent = 0;
+  , percent = 0
+  , blarf = new ArrayBuffer(250 * 1000 * 1000)
+  , blorb = new Uint8Array(blarf);
 
 /**
  * Event handler which fires when the parent thread sends us a
@@ -25,7 +27,7 @@ onmessage = function(evt) {
  * and pass it back every 1/5 of a second.
  */
 function doWork() {
-  interval = setInterval(updateStatus, 200);
+  interval = setInterval(updateStatus, 1);
 };
 
 /**
@@ -34,8 +36,15 @@ function doWork() {
  * down the thread.
  */
 function updateStatus() {
-  percent += 10;
-  postMessage({id: id, message: percent + '% completion'});
+  percent += 1;
+  for (var i = (2500000 * (percent-1)); i < (2500000 * percent); i++) {
+    blorb[i] = Math.floor(Math.random() * 256);
+  };
+
+  if (!(percent % 10)) {
+    postMessage({id: id, message: percent + '% completion'});
+  };
+  
   if (percent >= 100) {
     // we're done, so...
     finishWork();
@@ -49,8 +58,26 @@ function finishWork() {
   // get rid of the interval we started
   clearInterval(interval);
   // notify the parent thread that we're finished
-  postMessage({id: id, message: 'shutdown'});
-  // tell the browser it's OK to destroy this thread, so we don't leak
-  // resources
-  self.close();
+  postMessage({id: id, message: 'feed size: ' + blorb.length});
+  postMessage({id: id, message: 'feeding content'});
+
+  var pct = 0;
+  var intvl = setInterval(function() {
+    if (pct >= 100) {
+      clearInterval(intvl);
+      postMessage({id: id, message: 'shutdown'});
+      // tell the browser it's OK to destroy this thread, so we don't leak
+      // resources
+      self.close();
+      return;
+    };
+
+    if (!(pct % 10)) {
+      postMessage({id: id,
+                   message: (pct) + '% fed',
+                   percent: pct,
+                   content: blorb.slice(25000 * (pct-1), 25000 * pct)});
+    };
+    pct += 1;
+  }, 20);
 };
